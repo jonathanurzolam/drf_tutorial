bc9a1941d772b006a68b1a8fa3adf0c4e3974ac0
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2022-08-15 00:04:30 -0500

    Postman collection was added

--- user/viewsets.py
+++ user/viewsets.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
 from user.models import Customer
 from user.serializers import CustomerSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class CustomerViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and editing user instances.
     """
+    permission_classes = [IsAuthenticated]
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
